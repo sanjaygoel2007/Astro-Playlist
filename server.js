afaae883823f76b28aa1cae75d1e0b295c00d5a5
sanjaygoel2007 <sanjay.goel2007@gmail.com>
2025-12-11 00:53:16 +0530

    Remove CORS middleware from server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import { getDasha } from "./src/astrologyService.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Root check
