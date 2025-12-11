740ef59b6c7089ce564b181e8767db1a63e80bd2
sanjaygoel2007 <sanjay.goel2007@gmail.com>
2025-12-11 16:00:24 +0530

    Create page.js

---
+++ frontend/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Astro Playlist Frontend Running</h1>
+      <p>Your Next.js app is successfully deployed!</p>
+    </div>
+  );
+}
