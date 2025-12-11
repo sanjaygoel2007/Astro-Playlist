2008f9ee3162a2d4fa12f9b75a589c8f2516645a
sanjaygoel2007 <sanjay.goel2007@gmail.com>
2025-12-11 19:32:48 +0530

    Delete frontend/app/page.js

--- frontend/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Astro Playlist Frontend Running</h1>
-      <p>Your Next.js app is successfully deployed!</p>
-    </div>
-  );
-}
