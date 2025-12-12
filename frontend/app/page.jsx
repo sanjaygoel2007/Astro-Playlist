94aa6f71261e7cead5a5b3f282c4a26f7762841c
sanjaygoel2007 <sanjay.goel2007@gmail.com>
2025-12-12 13:23:25 +0530

    Update page.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -6,13 +6,6 @@ export default function Page() {
     <div className="container">
       <div className="card" style={{ marginBottom: 12 }}>
         <ClientForm />
-
-        <div className="small" style={{ marginTop: 16 }}>
-          Form posts to:{" "}
-          <code>
-            {process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"}/submit
-          </code>
-        </div>
       </div>
     </div>
   );
